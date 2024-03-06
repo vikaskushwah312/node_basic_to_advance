fd6d71d850670bac1ad0f06144c74861c1783049
vikas <vikas@itsabacus.com>
2024-03-06 16:09:22 +0530

    custom api response and error handling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,27 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app  = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+//parses incoming request bodies with JSON payloads
+app.use(express.json({ limit: "16kb"}))
+
+//To get the url in coded form (Browser url)
+app.use(express.urlencoded({
+    extended: true,
+    limit: "16kb"
+}))
+//to load the public folder
+app.use(express.static("public"))
+
+//to performa the curd for secure browser cookie
+// It extracts the cookies and attaches them to the request object as req.cookies.
+app.use(cookieParser())
 
 export {app}
