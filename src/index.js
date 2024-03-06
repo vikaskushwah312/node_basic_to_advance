81794fb63d793a0c97593d5127b2cdd4820004b9
vikas <vikas@itsabacus.com>
2024-03-06 14:31:27 +0530

    connect with databhase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
-console.log('ok I am index.js')
+import dotenv from "dotenv"
+import express from "express"
+import connectDB from  "./db/database.js"
+import { app } from "./app.js"
+
+dotenv.config({
+    path: "./.env"
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is runing at port : ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("Error", error)
+})
+
+
+
+
+// DB Connect code
+/*
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import {DB_NAME} from "./constants.js"
+
+dotenv.config({
+    path: './.env'
+})
+
+const app =  express()
+
+;( async ()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERROR ",error)
+        })
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`); 
+        })
+
+    } catch(error) {
+        console.error("error", error);
+        throw error
+    }
+})()
+ */
